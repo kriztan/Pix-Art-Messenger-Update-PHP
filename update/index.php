dedce56d6b577a559ef5550220dc9d14879bd9c3
Christian Schneppe <kriztan@users.noreply.github.com>
2020-10-24 13:55:22 +0200

    update updater

--- update/index.php
+++ update/index.php
@@ -1,9 +1,12 @@
 <?php
 ///////////////////////////////
+//// INPUT ////////////////////
 $versionCode = "184"; //184 = first versionCode with automatic updates via github, so no further changes needed.
+//////////////////////////////
+//// Code ////////////////////
 //// Get the string from the URL
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/kriztan/Pix-Art-Messenger/releases/latest');
+curl_setopt($ch, CURLOPT_URL, 'https://codeberg.org/api/v1/repos/kriztan/Pix-Art-Messenger/releases');
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -12,10 +15,21 @@ curl_close($ch);
 //// Decode the JSON string
 $data = json_decode($json);
 /////////////////////////////
-$downloadlink = $data->assets[1]->browser_download_url;
-$latestVersion = $data->tag_name;
-$changes = utf8_encode($data->body);
-$size = formatBytes($data->assets[1]->size);
+$counter = 0;
+$prerelease = true;
+$draft = true;
+for ($i = 0; $i <= 25; $i++) {
+	$prerelease = isBoolean($data[$i]->prerelease);
+	$draft = isBoolean($data[$i]->draft);
+	if (!$prerelease AND !$draft) {
+		$counter = $i;
+		break;
+	}
+}
+$downloadlink = $data[$counter]->assets[0]->browser_download_url;  // set to 1 if there is more than one apk
+$latestVersion = $data[$counter]->tag_name;
+$changes = utf8_encode($data[$counter]->body);
+$size = formatBytes($data[$counter]->assets[0]->size);  // set to 1 if there is more than one apk
 //////
 $downloadlink = strtolower($downloadlink);
 if (strlen($json) >= 1500) {
@@ -46,22 +60,30 @@ function formatBytes($bytes, $precision = 2) {
 }
 //////////////////////////////
 function getSize($url) {
-    // cache files are created like cache/abcdef123456...
-    $cacheFile = 'cache' . DIRECTORY_SEPARATOR . md5($url);
+	// cache files are created like cache/abcdef123456...
+	$cacheFile = 'cache' . DIRECTORY_SEPARATOR . md5($url);
 
-    if (file_exists($cacheFile)) {
-        $fh = fopen($cacheFile, 'r');
-        $size = fread($fh, filesize($cacheFile));
-       	if ($size > 0) {
-       		return $size;
-      	}
-    }
-		$size_raw[] = get_headers($url,1);
-    $cache = $size_raw[0]['Content-Length'];
-    $fh = fopen($cacheFile, 'w');
-    fwrite($fh, $cache);
-    fclose($fh);
-    return $cache;
+	if (file_exists($cacheFile)) {
+		$fh = fopen($cacheFile, 'r');
+		$size = fread($fh, filesize($cacheFile));
+		if ($size > 0) {
+			return $size;
+		}
+	}
+	$size_raw[] = get_headers($url,1);
+	$cache = $size_raw[0]['Content-Length'];
+	$fh = fopen($cacheFile, 'w');
+	fwrite($fh, $cache);
+	fclose($fh);
+	return $cache;
+}
+//////////////////////////////
+function isBoolean($value) {
+	if ($value) {
+		return true;
+	} else {
+		return false;
+	}
 }
 //////////////////////////////
 //////////////////////////////
